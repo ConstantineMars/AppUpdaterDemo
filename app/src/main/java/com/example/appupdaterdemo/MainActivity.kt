75492c7a91ce9f626312c5f264832faea2327afe
Constantine Mars <constantine.mars@gmail.com>
2020-03-02 15:21:34 -0800

    New version - 1.2

--- app/src/main/java/com/example/appupdaterdemo/MainActivity.kt
+++ app/src/main/java/com/example/appupdaterdemo/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.example.appupdaterdemo
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.github.javiersantos.appupdater.AppUpdater
 import com.github.javiersantos.appupdater.enums.Display
 import com.github.javiersantos.appupdater.enums.UpdateFrom
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        version.text = "Current version: ${BuildConfig.VERSION_NAME}"
 
         val appUpdater = AppUpdater(this)
                 .setDisplay(Display.DIALOG)
